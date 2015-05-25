2f5bee7358d6b7572e4f1465ade2dcda826ef8f7
Armand (Tydax) Bour <tydaxx@gmail.com>
2015-05-25 23:57:18 +0200

    Wrote the AJAX part for generating links on homepage

--- home.js
+++ home.js
@@ -1,6 +1,47 @@
-function generateDivLink(filename) {
+/*
+ * Script to load the data from users and display links
+ */
 
-    
+/* Load the data contained in the specified "filename" file and calls the specified function "callback" */
+function loadData(filename, callback) {
+    // Load file
+    var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    xhr.open('GET', filename, true);
+
+    // When given a response, interact with data
+    xhr.addEventListener('readystatechange', function() {
+        // Operation successful
+        if (xhr.readyState === 4 && xhr.status === "200") {
+           callback(xhr.responseText); 
+        }
+
+    }, false);
+
+    // Send request
+    xhr.send(null);
+}
+
+/* Returns a generated li block from the specified data object */
+function generateLinkLi(data) {
 
 }
+
+/* Function generating all the links */
+function generateAllLinks() {
+    var filename = "list_of_users.json";
+
+    loadData(filename, function(response) {
+        var users = JSON.parse(response);
+
+        for (var i = 0; i < Things.length; i++) {
+            Things[i]
+        };
+    });
+}
+
+/* Main function */
+(function() {
+    generateAllLinks();
+})();
 
