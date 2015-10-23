8027eb263c65048e6236aef4765718a09113d511
Armand (Tydax) Bour <tydaxx@gmail.com>
2015-10-23 23:59:34 +0200

    Overhauled the whole design

--- javascript/home.js
+++ javascript/home.js
@@ -1,9 +1,11 @@
 /*
  * Script to load the data from users and display links
+ *
  * @author: Armand (Tydax) BOUR
  */
 
 var currentUser;
+var userInfos = {};
 
 /* Load the data contained in the specified "filename" file and calls the specified function "callback" */
 function loadData(filename, callback) {
@@ -25,7 +27,27 @@ function loadData(filename, callback) {
     xhr.send(null);
 }
 
+function getCurrentDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd;
+    } 
+
+    if (mm < 10) {
+        mm = '0' + mm
+    } 
+
+    today = mm + '/' + dd + '/' + yyyy;
+
+    return today;
+}
+
 function calculateAge(birthDate, todayDate) {
+    var todayDate = getCurrentDate();
     var dataBirth = birthDate.split("/"),
         dataToday = todayDate.split("/"),
         age = dataToday[2] - dataBirth[2];
@@ -39,9 +61,41 @@ function calculateAge(birthDate, todayDate) {
     return age;
 }
 
+function generateUserInfoDiv(user) {
+    var divNode = document.createElement("div");
+    divNode.id = user.name;
+
+    // Create title element
+    var nameNode = document.createElement("h3");
+    nameNode.className += " user_info_name"
+    var nameText = document.createTextNode(user.name);
+    nameNode.appendChild(nameText);
+    divNode.appendChild(nameNode);
+
+
+    // Create age element
+    var birthDate = user.birthDate;
+    var age = calculateAge(birthDate);
+    var ageNode = document.createElement("p");
+    var ageText = document.createTextNode(age + " ans");
+    ageNode.className += " user_info_age";
+    ageNode.appendChild(ageText);
+    divNode.appendChild(ageNode);
+
+    // Create quote element
+    var quoteNode = document.createElement("p");
+    var quoteText = document.createTextNode("“" + user.quote + "”");
+    quoteNode.className += " user_info_quote";
+    quoteNode.appendChild(quoteText);
+    divNode.appendChild(quoteNode);
+
+    return divNode;
+}
+
 /* Returns a generated li block from the specified "user" object */
 function generateLinkLi(user) {
     var liNode = document.createElement("li");
+    liNode.id = "list_users_"
     liNode.className = user.className;
 
     // Create title element
@@ -51,48 +105,27 @@ function generateLinkLi(user) {
     nameNode.appendChild(nameText);
     liNode.appendChild(nameNode);
 
-    // Create age element
-    var birthDate = user.birthDate;
-    var todayDate = "28/07/2015";
-    var age = calculateAge(birthDate, todayDate);
-    var ageNode = document.createElement("span");
-    var ageText = document.createTextNode(" (" + age + " ans)");
-    ageNode.className += " listUsers_age invisible";
-    ageNode.appendChild(ageText);
-    nameNode.appendChild(ageNode);
-
-    // Create quote element
-    var quoteNode = document.createElement("p");
-    var quoteText = document.createTextNode("“" + user.quote + "”");
-    quoteNode.className += " listUsers_quote invisible";
-    quoteNode.appendChild(quoteText);
-    liNode.appendChild(quoteNode);
-
-
     // Add function when hovering
     liNode.addEventListener("mouseover", function() {
         liNode.className = user.className;
-        updateImage(user.imgUrl);
-        quoteNode.className = "listUsers_quote";
-        ageNode.className = "listUsers_age";
-
+        showUserInfo(user);
     }, false);
 
     liNode.addEventListener("mouseout", function() {
         liNode.className = "";
-        updateImage(currentUser
-                  ? currentUser.imgUrl
-                  : "../img/null.png");
-        quoteNode.className += " invisible";
-        ageNode.className += " invisible";
 
+        if (currentUser) {
+            showUserInfo(currentUser);
+        }
+        else {
+            updateImage("../img/null.png");
+        }
     }, false);
 
     // When clicking, keep it in focus
-    liNode.addEventListener("mouseclick", function() {
+    liNode.addEventListener("click", function() {
         currentUser = user;
     }, false);
-
 
     return liNode;
 }
@@ -100,6 +133,18 @@ function generateLinkLi(user) {
 function updateImage(src) {
     var imgNode = document.getElementById("info_user_pic");
     imgNode.src = src;
+}
+
+function showUserInfo(user) {
+    updateImage(user.imgUrl);
+    var baseInfo = document.getElementById("div_info_user_infos");
+
+    // Cleaning base info div
+    for (var i = 0; i < baseInfo.children.length; i++) {
+        baseInfo.removeChild(baseInfo.children[i]);
+    };
+
+    baseInfo.appendChild(userInfos[user.name]);
 }
 
 /* Generates all the links loading the file */
@@ -112,6 +157,7 @@ function generateAllLinks() {
         var listUsersNode = document.getElementById(listId);
 
         for (var i = 0; i < users.length; i++) {
+            userInfos[users[i].name] = generateUserInfoDiv(users[i]);
             var userNode = generateLinkLi(users[i]);
             listUsersNode.appendChild(userNode); 
         };
